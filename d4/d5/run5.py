cc6d2faf4790bb9bf048410f8917e1dde3ba2ab1
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2022-01-06 17:22:55 +0800

    commit

--- d4/d5/run5.py
+++ d4/d5/run5.py
@@ -1,3 +1,6 @@
 from d1.d2.d3.m3 import fun3
 
+
+# 这个文件在pychamr下完美运行，cmd命令行会报错 d1包找不到。除非在窗口会话设置PYTHONPATH环境变量
+# 不用硬编码sys.path.insert
 fun3()
---
+++ run_root.py
@@ -0,0 +1,5 @@
+from d1.d2.d3.m3 import fun3
+
+
+# 这个即使不设置 PYTHONPATH 环境变量  也不 sys.path.insert，在cmd可以运行。因为此运行起点文件在项目的根目录下。
+fun3()
